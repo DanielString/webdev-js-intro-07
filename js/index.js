bf99d2f875b1ce9f09ee89d5e4d0eeca307e5e01
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-01-14 21:17:52 -0500

    Added some JS code, but still lost

--- js/index.js
+++ js/index.js
@@ -2,3 +2,19 @@
 // that are not declared
 "use strict"
 
+// function vent() {
+//   console.log("I'm so lost")
+// }
+// vent()
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// let sum = add(5, 7);
+// console.log(sum);
+
+// function hello(name = 'Guest') {
+//   console.log('Hello ' + name + '?')
+// }
+// hello()
+// hello("Erin")
